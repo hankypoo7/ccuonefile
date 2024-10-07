74cfab3fec8ba424a32cb613da8c84c3c5b8940a
Hankypoo7 <100436822+hankypoo7@users.noreply.github.com>
2024-10-07 08:56:00 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
  * Ideally, this will be registered under the scope in uv.config.js so it will not need to be modified.
  * However, if a user changes the location of uv.bundle.js/uv.config.js or sw.js is not relative to them, they will need to modify this script locally.
  */
-importScripts('uv.bundle.js');
+importScripts('https://raw.githack.com/hankypoo7/ccuonefile/main/uv-bundle.js');
 importScripts('https://raw.githack.com/hankypoo7/ccuonefile/main/uv.config.js');
 importScripts('https://raw.githack.com/hankypoo7/ccuonefile/main/uv.sw.js');
 
